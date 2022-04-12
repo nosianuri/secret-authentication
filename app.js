5fdf79bcb0cdd39021fe27e8833dd7dbb33ccf8c
nosianuri <nurinosia@gmail.com>
2022-04-12 20:39:22 +0600

    hashing password set

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
  const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 
  app.use(express.static("public"));
@@ -26,7 +26,7 @@ console.log(process.env.API_KEY);
  });
 
 
- userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
  const User = new mongoose.model("User", userSchema);
 
@@ -46,7 +46,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -60,7 +60,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
 user.findOne({email: username}, function(err, foundUser){
   if(err) {
